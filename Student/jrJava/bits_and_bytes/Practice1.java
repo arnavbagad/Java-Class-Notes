6d4bcae4693729c5455751ba298144476dd0dfd3
arnavbagad <arnav.bagad@gmail.com>
2018-09-22 15:05:25 -0700

    Test
    Test commit

---
+++ Student/jrJava/bits_and_bytes/Practice1.java
@@ -0,0 +1,14 @@
+package jrJava.bits_and_bytes;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		int a = 2175;
+		
+		System.out.println(Integer.toBinaryString(a));
+		System.out.println(Integer.toHexString(a));
+		
+	}
+
+}
---
+++ Student/jrJava/bits_and_bytes/Practice2.java
@@ -0,0 +1,10 @@
+package jrJava.bits_and_bytes;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
